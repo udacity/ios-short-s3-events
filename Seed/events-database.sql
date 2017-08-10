c488b7b543d9d3375450fafe63ad55c9fbe8a6bb
Jarrod Parkes <parkesfjarrod@gmail.com>
2017-08-10 16:46:02 -0500

    update sql file to use charset for emoji

--- Seed/events-database.sql
+++ Seed/events-database.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.19, for Linux (x86_64)
 --
--- Host: 172.17.0.2    Database: game_on
+-- Host: 172.17.0.2    Database: game_night
 -- ------------------------------------------------------
 -- Server version	5.7.19
 
@@ -15,13 +15,14 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+ALTER DATABASE `game_night` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
 --
 -- Table structure for table `event_games`
 --
 
 DROP TABLE IF EXISTS `event_games`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `event_games` (
   `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
   `activity_id` int(6) unsigned NOT NULL,
@@ -29,8 +30,9 @@ CREATE TABLE `event_games` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `event_games` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 --
 -- Dumping data for table `event_games`
@@ -38,7 +40,11 @@ CREATE TABLE `event_games` (
 
 LOCK TABLES `event_games` WRITE;
 /*!40000 ALTER TABLE `event_games` DISABLE KEYS */;
-INSERT INTO `event_games` VALUES (7,1,1,'2017-07-24 20:43:51','2017-07-24 20:43:51'),(8,4,1,'2017-07-24 20:43:51','2017-07-24 20:43:51'),(9,8,1,'2017-07-24 20:43:51','2017-07-24 20:43:51'),(10,2,2,'2017-07-24 20:43:51','2017-07-24 20:43:51');
+INSERT INTO `event_games` VALUES
+(1,6,1,'2017-07-24 20:43:51','2017-07-24 20:43:51'),
+(2,5,1,'2017-07-24 20:43:51','2017-07-24 20:43:51'),
+(3,4,2,'2017-07-24 20:43:51','2017-07-24 20:43:51'),
+(4,6,2,'2017-07-24 20:43:51','2017-07-24 20:43:51');
 /*!40000 ALTER TABLE `event_games` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -47,12 +53,11 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `events`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `events` (
   `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
-  `emoji` varchar(25) DEFAULT NULL,
+  `emoji` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   `description` text,
   `host` int(6) unsigned NOT NULL,
   `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -61,8 +66,9 @@ CREATE TABLE `events` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `events` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 --
 -- Dumping data for table `events`
@@ -70,7 +76,9 @@ CREATE TABLE `events` (
 
 LOCK TABLES `events` WRITE;
 /*!40000 ALTER TABLE `events` DISABLE KEYS */;
-INSERT INTO `events` VALUES (7,'Always Look On the Bright Side of the Planet Earth','\\xf0\\x9f\\x94\\xa5','Its game night! Lets play some games!',1,'2017-08-01 14:53:25','San Francisco',1,'2017-08-01 14:53:25','2017-07-24 20:43:51'),(8,'Event 2','\\xf0\\x9f\\x87\\xba\\xf0\\x9f\\x87\\xb8','Another event description',1,'2017-08-01 14:54:14','Huntsville',1,'2017-08-01 14:54:14','2017-07-24 20:43:51');
+INSERT INTO `events` VALUES
+(1,'Always Look On the Bright Side of the Planet Earth','','Its game night! Lets play some games!',1,'2017-08-01 14:53:25','San Francisco',1,'2017-08-01 14:53:25','2017-07-24 20:43:51'),
+(2,'Event 2','','Another event description',1,'2017-08-01 14:54:14','Huntsville',1,'2017-08-01 14:54:14','2017-07-24 20:43:51');
 /*!40000 ALTER TABLE `events` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,8 +87,7 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `rsvps`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `rsvps` (
   `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(6) unsigned NOT NULL,
@@ -90,8 +97,7 @@ CREATE TABLE `rsvps` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `rsvps`
@@ -99,11 +105,13 @@ CREATE TABLE `rsvps` (
 
 LOCK TABLES `rsvps` WRITE;
 /*!40000 ALTER TABLE `rsvps` DISABLE KEYS */;
-INSERT INTO `rsvps` VALUES (7,1,1,1,'Ill be there!','2017-07-24 20:43:51','2017-07-24 20:43:51'),(8,2,1,1,'Ill be there!','2017-07-24 20:43:51','2017-07-24 20:43:51');
+INSERT INTO `rsvps` VALUES
+(1,1,1,1,'Ill be there!','2017-07-24 20:43:51','2017-07-24 20:43:51'),
+(2,2,1,1,'Im coming to the event!','2017-07-24 20:43:51','2017-07-24 20:43:51');
 /*!40000 ALTER TABLE `rsvps` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
